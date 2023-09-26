7256112be838a523c59872d9c03d05a87ac22a52
Re-Santos <renata.prog@gmail.com>
2023-09-26 15:44:16 -0300

    Exercício1

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,21 @@ import {
     getAllProducts,
     searchProductsByName
 } from './database';
+import express, {Request, Response} from 'express';
+import cors from 'cors'
 
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003")
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.send('Pong!')
+})
 
 console.log("Usuários:", users);
 console.log("Produtos:", products);
