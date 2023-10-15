36809d3b18145ce1f372117e5a0c1dff7dff5146
Re-Santos <renata.prog@gmail.com>
2023-10-15 17:57:46 -0300

    Exercicio1 relacoes-sql-i

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -19,9 +19,12 @@ VALUES ('u001', 'Fulano', 'fulano@email.com', 'fulano123',datetime('now')),
 INSERT INTO users 
 VALUES ('u004', 'Amanda', 'amanda@email.com', 'amanda123', datetime('now')),
        ('u005', 'Jéssica', 'jessica@email.com', 'jessica123',datetime('now')),
-       ('u006', 'Beatriz', 'beatriz@email', 'beatriz123', datetime('now')),
-       ('u007', 'Cristiane','cris@email.com','cris159', datetime('now'));
-
+       ('u006', 'Beatriz', 'beatriz@email', 'beatriz123', datetime('now'));
+INSERT INTO users
+VALUES ('u007', 'Cristiane','cris@email.com','cris159', datetime('now'));
+       
+DELETE FROM users 
+WHERE id = 'u001';
 
 CREATE TABLE products(
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
@@ -35,11 +38,31 @@ SELECT * FROM products;
 
 INSERT INTO products(id, name, price, description, image_url)
 VALUES ('prod001', 'Mouse gamer', 250, 'Melhor mouse do mercado','https://picsum.photos/seed/Mouse%20gamer/400'),
-      ('prod002', 'SSD gamer',500, 'Acelere seu sistema com velocidades incríveis de leitura e gravação', 'https://images.unsplash.com/photo' ),
-      ('prod003', 'Cadeira gamer',547.19, 'Cadeira ergonômica, reclinável, couro sintético','https://http2.mlstatic.com/D_NQ_NP_921709-MLB70326385749_072023-O.webp'),
-      ('prod004', 'Teclado gamer',223, 'Teclado multimídia, mecânico, led','https://http2.mlstatic.com/D_NQ_NP_880113-MLB50584015972_072022-O.webp'),
-      ('prod005', 'Monitor', 265, 'Monitor Firemax led 17 com Hdmi, vga','https://http2.mlstatic.com/D_NQ_NP_789192-MLB71931725165_092023-W.webp'),
-      ('prod006','Mochila notebook', 89.73, 'Mochila impermeável para notebook, espaçosa, preta', 'https://http2.mlstatic.com/D_NQ_NP_824854-MLB70656406276_072023-W.webp');
+       ('prod002', 'SSD gamer',500, 'Acelere seu sistema com velocidades incríveis de leitura e gravação', 'https://images.unsplash.com/photo' ),
+       ('prod003', 'Cadeira gamer',547.19, 'Cadeira ergonômica, reclinável, couro sintético','https://http2.mlstatic.com/D_NQ_NP_921709-MLB70326385749_072023-O.webp'),
+       ('prod004', 'Teclado gamer',223, 'Teclado multimídia, mecânico, led','https://http2.mlstatic.com/D_NQ_NP_880113-MLB50584015972_072022-O.webp'),
+       ('prod005', 'Monitor', 265, 'Monitor Firemax led 17 com Hdmi, vga','https://http2.mlstatic.com/D_NQ_NP_789192-MLB71931725165_092023-W.webp'),
+       ('prod006','Mochila notebook', 89.73, 'Mochila impermeável para notebook, espaçosa, preta', 'https://http2.mlstatic.com/D_NQ_NP_824854-MLB70656406276_072023-W.webp');
+
+INSERT INTO products
+VALUES ('prod007','Fone de ouvido', 59.99, 'Fone headset business, ideal para quem trabalha ao longo do dia, alta qualidade de som', 'https://http2.mlstatic.com/D_NQ_NP_836340-MLB32003623445_082019-V.webp');
 
 SELECT * FROM products 
 WHERE name LIKE '%gamer%';
+DELETE FROM products 
+WHERE id = 'prod001';
+
+UPDATE products
+SET name = 'Mouse led',
+    price= 29,
+    description='Mouse recarregável Wireles, ergônomico',
+    image_url='https://http2.mlstatic.com/D_NQ_NP_601758-MLB70301835457_072023-W.webp'
+WHERE id ='prod003';
+
+CREATE TABLE purchases(
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    buyer TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    create_at TEXT NOT NULL,
+    FOREIGN KEY (buyer) REFERENCES users(id)
+)
